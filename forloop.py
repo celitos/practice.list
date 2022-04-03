3c093acb9676ca62ceb8172019a4d47028707fc6
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-03 20:45:45 -0300

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,14 @@
+
+#print from 1 to 5
+
+#simple sequential
+# here index is not included #
+#print('1')
+#print('2')
+#print('3')
+#print('4')
+#print('5')
+
+#there it is
+for number in range(1, 10):
+  print(number)
