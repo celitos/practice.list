cf24b3bca91ad8c749d60ef97c163687ab059f55
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-03 20:58:26 -0300

    Create forloop_if.py

---
+++ forloop_if.py
@@ -0,0 +1,10 @@
+sale_completed = False
+sale_data = 'Sale completed: U$47.50'
+
+for send in range(3):
+  if sale_completed:
+    print(sale_data)
+    print('Additional Information sent to your email address')   
+    break 
+else:
+  print('Sale not completed.')
