6b855b9e2d3a79b3504f0403164ac64c8fa2b6b4
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-03 20:13:37 -0300

    Create test_if else and elif.py

---
+++ test_if else and elif.py
@@ -0,0 +1,10 @@
+speed = 64
+
+if speed > 75:
+  print('high speed detected')
+  print('Please slow down for safety reasons!!')
+elif speed < 60:
+  print('Please drive above 65mph')
+else:
+  print('Speed is okay :)')
+ 
