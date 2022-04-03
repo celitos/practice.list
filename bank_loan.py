342454682312068f0a729b710ec86799ccec52ab
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-03 20:22:23 -0300

    Create bank_loan.py

---
+++ bank_loan.py
@@ -0,0 +1,9 @@
+yearly_income_85k = True
+credit_score = True
+
+##i can include and/or...but it changes the final statement##
+
+if yearly_income_85k or credit_score:
+  print('Loan Approved')
+else:
+  print('Process Denied')
