fa4072c16a1dcd6fa60b743f2a9a6ab96f772122
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-03 20:28:44 -0300

    Create ternary_test.py

---
+++ ternary_test.py
@@ -0,0 +1,6 @@
+#document approval request, user has 16 years old and some extra information are required, this one is a ternary bc i need to reduce my code line
+age = 10
+
+result = 'Document Approved' if age >=15 else 'Document Denied'
+
+print(result)
