3341e1281d124ee576d438baa4a62c42f1c5d719
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-04 21:57:21 -0300

    Create food_counter.py

---
+++ food_counter.py
@@ -0,0 +1,22 @@
+food_counter = 24
+
+if food_counter ==26:
+  print('good, 26 pieces!!')
+elif food_counter ==25:
+  print('good, 25 pieces!!')
+elif food_counter ==24:
+  print('good, 24 pieces!!')
+elif food_counter ==23:
+  print('good, 23 pieces!!')
+elif food_counter ==22:
+  print('good, 22 pieces!!')
+elif food_counter ==21:
+  print('good, 21 pieces!!')
+elif food_counter ==20:
+  print('good, 20 pieces!!')
+elif food_counter ==19:
+  print('good, 19 pieces!!')
+elif food_counter ==18:
+  print('good, 18 pieces!!')
+else:
+  print('Ok!')
