cabcdbc8a7212b90754719182290048962a3a676
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-05 22:32:09 -0300

    Create input-print.py

---
+++ input-print.py
@@ -0,0 +1,7 @@
+carro = input("Qual o seu carro?" )
+ano = input("Qual o ano de fabricação" )
+cor = input("Qual a cor de seu carro" )
+
+print("Seu carro é do modelo "+ carro)
+print("O ano de fabricação é "+ ano)
+print("Seu carro tem a cor "+ cor)
