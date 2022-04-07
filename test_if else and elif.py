703b78ddc223461f927e021db20b60df303f74ab
atomic <93286134+atomic9@users.noreply.github.com>
2022-04-06 22:42:08 -0300

    Update test_if else and elif.py

--- test_if else and elif.py
+++ test_if else and elif.py
@@ -5,6 +5,8 @@ if speed > 75:
   print('Please slow down for safety reasons!!')
 elif speed < 60:
   print('Please drive above 65mph')
+elif speed == 61:
+  print('Nice speed, but keep up to 65mph')
 else:
   print('Speed is okay :)')
  
