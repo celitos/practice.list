feef748ba13d09e1ed5b62b943e6b86560963492
atomic <93286134+atomic9@users.noreply.github.com>
2022-05-23 14:24:51 -0300

    Create quackquack1functions.py
    this is a code for functions will be modified some day :o

---
+++ quackquack1functions.py
@@ -0,0 +1,19 @@
+#functions lol i know 
+
+def somar():
+  num1 = 10
+  num2 = 15
+  result = num1 + num2
+  print(result)
+  
+def somar1():
+  num1 = 10
+  num2 = 15
+  result = num1 + num2
+  print(result)
+
+
+somar()
+somar1()# this here will be ok to python
+
+print(result) #only this will not be defined 
