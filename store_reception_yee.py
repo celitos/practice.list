fbecaea6a1ab3f72dacd8be029536b0be10f09a3
atomic <93286134+atomic9@users.noreply.github.com>
2022-05-23 14:51:28 -0300

    Create store_reception_yee.py
    Jack and Jill needs laptops :)

---
+++ store_reception_yee.py
@@ -0,0 +1,49 @@
+def welcome(name, quantity):
+  print(f'Hello{name}.')
+  print(f'We have {str{quantity} laptops stored')
+
+welcome('Waldo', 5)
+welcome('Jack', 4)
+welcome('Jill', 2)
+
+
+  def welcome(brand, loan):
+print(f'Welcome{brand}.')
+print(f'We have avaliable for your business upgrade {loan} dollars')
+
+welcome('Tes', 3855000000)
+welcome('Microsof', 278548000)
+welcome('Goog', 2655000000)
+welcome('Bank of Amer', 2625000000)
+welcome('For', 2587000000)
+welcome('Chevroole', 2569100000)
+welcome('Adob', 255000000)
+welcome('Tesc', 2545000000)
+welcome('Carrefo', 2525000000)
+welcome('H&?', 2505000000)
+welcome('Samsu', 2455000000)
+welcome('App', 2355000000)
+welcome('Sonyyy', 2255000000)
+welcome('Philp', 2055000000)
+welcome('Onboard', 1355000000)
+
+
+'''
+def welcome_Waldo():
+  print('Hello Waldo')
+  print('We have 5 laptops stored')
+  
+
+def welcome_Jack():
+  print('Hello Jim')
+  print('We have 4 laptops stored')
+  
+  
+def welcome_Jill():
+  print('Hello Jill')
+  print('We have 2 laptops stored')
+  
+welcome_Waldo()
+welcome_Jack()
+welcome_Jill()
+'''
